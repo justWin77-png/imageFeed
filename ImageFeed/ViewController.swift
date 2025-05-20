766dd05adb0410b5955dee2eb63efa7822004ab8
justWin <evseev693@gmail.com>
2025-05-20 21:25:43 +0500

    Создан проект

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Данил Евсеев on 5/19/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
 
