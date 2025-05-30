cf8c4e4ac901f62eefd993dfbb5757bbe707dda8
justWin <evseev693@gmail.com>
2025-05-30 18:08:58 +0500

    Добавим UITabBarController в Xcode

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UIView!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction func didTaplogoutButton() {
+    }
+}
