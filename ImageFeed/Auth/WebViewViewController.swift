fa06479e4ba428e4a62e515b9590635550ffbe02
justWin <evseev693@gmail.com>
2025-06-24 00:13:53 +0500

    Доработки номер 1

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -13,7 +13,7 @@ final class WebViewViewController: UIViewController {
     @IBOutlet private var progressView: UIProgressView!
 
     weak var delegate: WebViewViewControllerDelegate?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,10 +21,10 @@ final class WebViewViewController: UIViewController {
 
         var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
         urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: accessKey),
-            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
             URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: accessScope)
+            URLQueryItem(name: "scope", value: Constants.accessScope)
         ]
         let url = urlComponents.url!
 
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -1,7 +1,14 @@
 import Foundation
 
-let accessKey = "L4dtaPMb7dhPhdWkPyLpM-xi8Av5HhRzHqoTXJMw--s"
-let secretKey = "gwtWzsW5XdQnOROU-HCRixyaF2gjXKRDbd5_FTzAzmA"
-let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-let accessScope = "public+read_user+write_likes"
-let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+enum Constants {
+    static let accessKey = "L4dtaPMb7dhPhdWkPyLpM-xi8Av5HhRzHqoTXJMw--s"
+    static let secretKey = "gwtWzsW5XdQnOROU-HCRixyaF2gjXKRDbd5_FTzAzmA"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseUR = {
+        guard let baseURL = URL(string: "https://api.unsplash.com") else {
+            assertionFailure("Invalid base URL")
+            return
+        }
+    }
+}
